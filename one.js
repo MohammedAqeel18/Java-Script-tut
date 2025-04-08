1f93544eef85d07f18c552ee4c05251641f6a7af
MohammedAqeel18 <aqeelworkmail@gmail.com>
2025-04-08 10:28:26 +0530

    update

--- one.js
+++ one.js
@@ -1,3 +1,13 @@
 function addFiles(){
+ 
+    var files = document.getElementById('files').files;
+    var fileList = document.getElementById('fileList');
+    fileList.innerHTML = ''; // Clear the list before adding new files
+    
+    for (var i = 0; i < files.length; i++) {
+        var fileItem = document.createElement('li');
+        fileItem.textContent = files[i].name;
+        fileList.appendChild(fileItem);
+    }   
     
 }
