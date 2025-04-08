1a153d9f9987cb8f8352983a688f988c98fc75da
MohammedAqeel18 <aqeelworkmail@gmail.com>
2025-04-08 17:45:24 +0530

    updated

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,16 @@
-console.log("Hello World!");
-console.log(typeof "Aqeel");
-console.log(typeof 5);
-console.log(typeof true);
+const myVariable= "Mathematics";
+
+console.log(myVariable.length);
+console.log(myVariable.charAt(5));
+console.log(myVariable.indexOf("ma"));
+console.log(myVariable.lastIndexOf("s"));
+console.log(myVariable.slice(0, 5));
+console.log(myVariable.toUpperCase());
+console.log(myVariable.toLowerCase());
+console.log(myVariable.toLocaleUpperCase());
+console.log(myVariable.toLocaleLowerCase());
+console.log(myVariable.split("m"));
+console.log(myVariable.split(""));
+console.log("Aqeel , Hafsa , Bisharath".split(","))
+console.log(myVariable.replace("Mathematics", "English"))
+console.log(myVariable.toUpperCase().replace("M", "m"))
